98278aeac0011ab3d9a1b3bd8ecc25425598c435
Qubot <1445788683@qq.com>
2021-03-04 17:20:00 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -123,9 +123,8 @@ namespace triodecar {
     //% blockId=motor_MotorRun block="motor|%motor|move Foward at speed|%speed"
     //% speed.min=0 speed.max=10
     //% motor.fieldEditor="gridpicker" motor.fieldOptions.columns=2
-    export function motorRun(Motor: motor, speed: number): void {
-        speed = 5
-        let pulsetime = Math.map(speed, 0, 10, 10000, 0)
+    export function motorRun(Motor: motor, speed: number = 5): void {
+        let pulsetime = Math.map(speed, 0, 10, 12000, 0)
         if (Motor == motor.left) {
             pins.servoSetPulse(AnalogPin.P14, pulsetime)
         } 
