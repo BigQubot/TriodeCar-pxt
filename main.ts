398c93b57ea8d284d2cc808654dd4a31a98e9e69
Qubot <1445788683@qq.com>
2021-03-05 15:44:02 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -34,41 +34,6 @@ namespace triodecar {
     }
 
 
-    /**
-     * Read ultrasonic sensor.
-     */
-
-    //% blockId=ultrasonic_sensor block="read ultrasonic sensor |%unit "
-    //% weight=95
-    export function Ultrasonic(unit: PingUnit, maxCmDistance = 500): number {
-        let d
-        pins.digitalWritePin(DigitalPin.P12, 1);
-        basic.pause(1)
-        pins.digitalWritePin(DigitalPin.P12, 0);
-        if (pins.digitalReadPin(DigitalPin.P13) == 0) {
-            pins.digitalWritePin(DigitalPin.P12, 0);
-            //sleep_us(2);
-            pins.digitalWritePin(DigitalPin.P12, 1);
-            //sleep_us(10);
-            pins.digitalWritePin(DigitalPin.P12, 0);
-            d = pins.pulseIn(DigitalPin.P13, PulseValue.High, maxCmDistance * 58);//readPulseIn(1);
-        } else {
-            pins.digitalWritePin(DigitalPin.P12, 0);
-            pins.digitalWritePin(DigitalPin.P12, 1);
-            d = pins.pulseIn(DigitalPin.P13, PulseValue.Low, maxCmDistance * 58);//readPulseIn(0);
-        }
-        let x = d / 39;
-        if (x <= 0 || x > 500) {
-            return 0;
-        }
-        switch (unit) {
-            case PingUnit.Centimeters: return Math.round(x);
-            default: return Math.idiv(d, 2.54);
-        }
-
-    }
-
-
      /**
      * Read line tracking sensor.
      */
@@ -141,5 +106,41 @@ namespace triodecar {
                 pins.servoSetPulse(AnalogPin.P15, pulsetime)
             } 
         } 
+    }
+
+
+    /**
+     * Read ultrasonic sensor.
+     */
+
+    //% blockId=ultrasonic_sensor block="read ultrasonic sensor |%unit "
+    //% weight=95
+    //% block advanced=true
+    export function Ultrasonic(unit: PingUnit, maxCmDistance = 500): number {
+        let d
+        pins.digitalWritePin(DigitalPin.P12, 1);
+        basic.pause(1)
+        pins.digitalWritePin(DigitalPin.P12, 0);
+        if (pins.digitalReadPin(DigitalPin.P13) == 0) {
+            pins.digitalWritePin(DigitalPin.P12, 0);
+            //sleep_us(2);
+            pins.digitalWritePin(DigitalPin.P12, 1);
+            //sleep_us(10);
+            pins.digitalWritePin(DigitalPin.P12, 0);
+            d = pins.pulseIn(DigitalPin.P13, PulseValue.High, maxCmDistance * 58);//readPulseIn(1);
+        } else {
+            pins.digitalWritePin(DigitalPin.P12, 0);
+            pins.digitalWritePin(DigitalPin.P12, 1);
+            d = pins.pulseIn(DigitalPin.P13, PulseValue.Low, maxCmDistance * 58);//readPulseIn(0);
+        }
+        let x = d / 39;
+        if (x <= 0 || x > 500) {
+            return 0;
+        }
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.round(x);
+            default: return Math.idiv(d, 2.54);
+        }
+
     }
 }
